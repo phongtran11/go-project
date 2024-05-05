646ac135f64a75ff1b8dc5f12f6277dd6f85ded9
phongNarin <phong.tran@sungrove.co.jp>
2024-05-05 11:54:08 +0700

    update load .env & connect db

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/phongtran11/go-project/database"
 	"github.com/phongtran11/go-project/routes"
 )
 
 func main() {
+	// load dotenv
+	err := godotenv.Load()
+
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
 	// init connection to database
 	database.ConnectDB()
 
--- database/init_connection.go
+++ database/init_connection.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/phongtran11/go-project/models"
 	"gorm.io/driver/mysql"
@@ -12,16 +13,36 @@ type TDatabaseStore struct {
 	DB *gorm.DB
 }
 
+type TDBConfig struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	Database string
+}
+
 var databaseStore TDatabaseStore
 
 func ConnectDB() {
+	dbConfig := &TDBConfig{
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Database: os.Getenv("DB_DATABASE"),
+	}
+
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbConfig.Username, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database)
+
+	fmt.Print(dns)
+
 	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       "root:root@tcp(localhost:3306)/go_project?charset=utf8&parseTime=True&loc=Local", // data source name
-		DefaultStringSize:         256,                                                                              // default size for string fields
-		DisableDatetimePrecision:  true,                                                                             // disable datetime precision, which not supported before MySQL 5.6
-		DontSupportRenameIndex:    true,                                                                             // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
-		DontSupportRenameColumn:   true,                                                                             // `change` when rename column, rename column not supported before MySQL 8, MariaDB
-		SkipInitializeWithVersion: false,                                                                            // auto configure based on currently MySQL version
+		DSN:                       dns,   // data source name
+		DefaultStringSize:         256,   // default size for string fields
+		DisableDatetimePrecision:  true,  // disable datetime precision, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,  // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,  // `change` when rename column, rename column not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
 	}), &gorm.Config{})
 
 	if err != nil {
